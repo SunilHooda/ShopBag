7059f6c8f290369e6792c1162d95398e1fa228b5
Sunil Hooda <Sunilhooda000@gmail.com>
2024-09-15 20:40:14 +0530

    Stripe Config file updated

--- backend/config/stripe.js
+++ backend/config/stripe.js
@@ -1,5 +1,6 @@
-const Stripe = require("stripe");
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
-const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
+const stripeSecretKey = `${process.env.STRIPE_SECRET_KEY}`;
+console.log(stripeSecretKey);
 
 module.exports = stripe;
